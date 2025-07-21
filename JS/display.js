3b6f09a106f744ef8240156021f006100ddcbb9a
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-21 09:39:26 +0530

    updated

--- JS/display.js
+++ JS/display.js
@@ -1,5 +1,13 @@
 // Displaying contents
 
 console.log(18);
+console.log(typeof 18);
 console.log(typeof Hello);
 console.log(typeof "Hello");
+console.log(typeof false);
+
+console.log();
+
+console.log("ECMA", 6);
+console.log("ECMA"+ 6);
+console.log(6 + 6);
---
+++ JS/var.js
@@ -0,0 +1,28 @@
+
+console.log(firstData);
+
+var firstData
+console.log(firstData, typeof firstData);
+
+firstData = 17
+console.log(firstData, typeof firstData);
+
+var firstData = "Hello"
+console.log(firstData, typeof firstData);
+
+// console.log(secondData);
+
+const secondData = "World"
+console.log(secondData, typeof secondData);
+
+
+var i = 10;
+console.log("Before loop", i);
+
+for (let i = 1; i <= 3; i++ ){
+    console.log("Inside loop", i)
+}
+
+console.log("After loop", i);
+
+console.log(`variables we used in this program are ${firstData}, ${secondData}`);
