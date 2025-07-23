b5fbe0726f0ed660d0fabebba19316af4b29e880
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-23 09:26:21 +0530

    updated

--- JS/if1.js
+++ JS/if1.js
@@ -8,27 +8,3 @@ if (age >= 18){
     console.log('Not Eligible for Voting');
 }
 
-//w.a.p program to check a number is +ve or -ve
-
-const num = 12;
-
-if (num > 0){
-    console.log(` ${num} is +ve`);
-} else if (num < 0){
-    console.log(` ${num} is -ve`);
-} else{
-    console.log(` ${num} is zero`);
-}
-
-//w.a.p program to check largest of 2 numbers
-
-const x = 10;
-const y = 20;
-
-if (x > y){
-    console.log(` ${x} is largest`);
-} else if (y > x){
-    console.log(` ${y} is largest`);
-} else{
-    console.log(` Both are equal`);
-}
---
+++ JS/if2.js
@@ -0,0 +1,24 @@
+//w.a.p program to check a number is +ve or -ve
+
+const num = 12;
+
+if (num > 0){
+    console.log(` ${num} is +ve`);
+} else if (num < 0){
+    console.log(` ${num} is -ve`);
+} else{
+    console.log(` ${num} is zero`);
+}
+
+//w.a.p program to check largest of 2 numbers
+
+const x = 10;
+const y = 20;
+
+if (x > y){
+    console.log(` ${x} is largest`);
+} else if (y > x){
+    console.log(` ${y} is largest`);
+} else{
+    console.log(` Both are equal`);
+}
