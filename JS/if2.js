a1b8f075143916765707409fa841e9b2499206d4
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-23 09:31:31 +0530

    updated

--- JS/if2.js
+++ JS/if2.js
@@ -10,15 +10,3 @@ if (num > 0){
     console.log(` ${num} is zero`);
 }
 
-//w.a.p program to check largest of 2 numbers
-
-const x = 10;
-const y = 20;
-
-if (x > y){
-    console.log(` ${x} is largest`);
-} else if (y > x){
-    console.log(` ${y} is largest`);
-} else{
-    console.log(` Both are equal`);
-}
---
+++ JS/if3.js
@@ -0,0 +1,12 @@
+//w.a.p program to check largest of 2 numbers
+
+const x = 10;
+const y = 20;
+
+if (x > y){
+    console.log(` ${x} is largest`);
+} else if (y > x){
+    console.log(` ${y} is largest`);
+} else{
+    console.log(` Both are equal`);
+}
