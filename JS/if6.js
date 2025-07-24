4875f3acf29e52904ab839d8aa37317f63146df5
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-24 09:52:03 +0530

    updated

--- JS/if6.js
+++ JS/if6.js
@@ -1,8 +1,8 @@
 // WAP to print second largest number among 3 unique number and print them in ascending order
 
-let num1 = 100, num2 = 60, num3 = 70;
+let num1 = 100, num2 = 60, num3 = 55;
 
-console.log(`Before sorting ${num1}, ${num2}, ${num3}`);
+// console.log(`Before sorting ${num1}, ${num2}, ${num3}`);
 
 a = (num1 > num2) && (num1 < num3) || (num1 < num2) && (num1 > num3) ? [num1 ,num2] = [num2, num1] : (num2 > num1) && (num2 < num3) || (num2 < num1) && (num2 > num3) ? true : (num3 > num1) && (num3 < num2) || (num3 < num1) && (num3 > num2) ? [num2 ,num3] = [num3, num2] : false ;
 
