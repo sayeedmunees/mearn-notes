9b26e5eb53b31cc2199505082619e53ba8997da0
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-24 09:09:06 +0530

    updated

--- JS/if5.js
+++ JS/if5.js
@@ -6,3 +6,4 @@ num = 18;
 console.log((num%15) == 0 ? "fizzbuzz" : (num%5) == 0 ? "buzz" : (num%3) == 0 ? "fizz"  : "not divisible by 3, 5 & 15");
 
 
+
---
+++ JS/if6.js
@@ -0,0 +1,20 @@
+// WAP to print second largest number among 3 unique number and print them in ascending order
+
+num1 = 100;
+num2 = 60;
+num3 = 70;
+
+console.log(`Before sorting ${num1}, ${num2}, ${num3}`);
+
+a = (num1 > num2) && (num1 < num3) || (num1 < num2) && (num1 > num3) ? [num1 ,num2] = [num2, num1] : (num2 > num1) && (num2 < num3) || (num2 < num1) && (num2 > num3) ? true : (num3 > num1) && (num3 < num2) || (num3 < num1) && (num3 > num2) ? [num2 ,num3] = [num3, num2] : false ;
+
+console.log( a ? `${num2} Second largest` : `3 unique numbers are expected`);
+console.log( a && num1 > num3 ? `After sorting ${num3}, ${num2}, ${num1}` : a ? `After sorting ${num1}, ${num2}, ${num3}` : ``);
+
+
+
+// a = (num1 > num2) && (num1 < num3) || (num1 < num2) && (num1 > num3)
+// b = (num2 > num1) && (num2 < num3) || (num2 < num1) && (num2 > num3)
+// c = (num3 > num1) && (num3 < num2) || (num3 < num1) && (num3 > num2)
+
+// console.log( a ? `${num1} Second largest` : b ? `${num2} Second largest` : c ? `${num3} Second largest` : `Enter unique number`);
