fe1cd65e6a1ffb89882c56a30382472bb4fa0f76
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-24 10:01:42 +0530

    updated

--- JS/if4.js
+++ JS/if4.js
@@ -1,7 +1,10 @@
 //  w.a.p to display a number if its greater than 10
 
-x = 11;
+x = 10;
 if (x > 10) {
     console.log(`Number ${x} is greater than 10`);
 }
 
+console.log(x > 10 && `Number ${x} is greater than 10`);
+
+// console.log(x > 10 ? `Number ${x} is greater than 10` : false);
