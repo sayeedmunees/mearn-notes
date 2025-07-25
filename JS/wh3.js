e546d194addf19709a4934bcce1dce5e60b7fad0
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-25 10:21:30 +0530

    updated

--- JS/wh3.js
+++ JS/wh3.js
@@ -1,6 +1,6 @@
 // wap to find factorial of a number
-// import prompt
 
+// import prompt
 const prompt = require('prompt-sync')({sigint:true})
 
 var input = prompt("Enter any number: ")
@@ -11,6 +11,6 @@ var num = Number(input)
 var fact = 1;
 while (num>0) {
     fact *= num;
-    num--
+    num--;
 }
 console.log(`Factorial of ${input} is ${fact}`);
