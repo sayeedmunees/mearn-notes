7e44a059a193a3808f717c93fe9f4954721fee31
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-26 10:56:40 +0530

    updated

--- JS/for7.js
+++ JS/for7.js
@@ -9,10 +9,10 @@ const input2 = prompt("Enter a 2nd number : ");
 var num1 = Number(input1);
 var num2 = Number(input2);
 
-const smallNumber = num1 > num2 ? num1 : num2;
+// const smallNumber = num1 > num2 ? num1 : num2;
 
 var hcf = 1;
-for (let i = 1 ; i <= smallNumber ; i++) {
+for (let i = 1 ; i <= (num1 > num2 ? num1 : num2) ; i++) {
     if (num1%i == 0 && num2%i == 0) {
         hcf = i;
     }
