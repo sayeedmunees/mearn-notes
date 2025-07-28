5143f48452c68c9c42dae007d9634b626c931d57
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-28 10:12:11 +0530

    updated

---
+++ JS/fn1.js
@@ -0,0 +1,9 @@
+function add(num1 , num2 ){
+    result = num1 +num2;
+    console.log(`Result ${result}`);
+    return result;
+}
+
+const result1 = add(10, 5);
+console.log(`Result from function${result1}`);
+// console.log(`Result from function${add(10,5)}`);
