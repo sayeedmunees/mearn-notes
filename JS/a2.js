60095f884f3cbdca923f172f7d87d6edbdb86adf
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-29 10:54:14 +0530

    updated

---
+++ JS/a2.js
@@ -0,0 +1,22 @@
+// find out
+
+var expenses = [23000, 45000, 55000, 10000, 22000, 12000, 97000]
+
+// Display least expense
+// Display Highest expense
+// Display total expense
+
+var leastExpense = expenses[0];
+var highestExpense = expenses[0];
+var totalExpense = 0;
+for (let i of expenses){
+    if ( leastExpense > i){
+        leastExpense = i
+    }
+    else if (highestExpense < i){
+        highestExpense = i
+    }
+    totalExpense += i ;
+}
+
+console.log(`Least expense: ${leastExpense}, Highest expense: ${highestExpense}, Total expense: ${totalExpense}`);
