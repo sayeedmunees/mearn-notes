0d276f92128000b0bfcff6d86f06e010b22e09a8
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-29 10:28:26 +0530

    updated

--- JS/a1.js
+++ JS/a1.js
@@ -22,3 +22,21 @@ console.log(days);
 
 days.shift();
 console.log(days);
+
+// Iteration
+
+
+// Normal for loop
+for (let index = 0; index < days.length; index++) {
+    console.log(days[index]);
+}
+
+// For -of loop
+for (let day of days) {
+   console.log(day);
+}
+
+// For -in loop
+for (let index in days) {
+    console.log(days[index]);
+}
