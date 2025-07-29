abe004719bf32f81ed17dc899bbfdc64cb47718c
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-29 10:17:36 +0530

    updated

---
+++ JS/a1.js
@@ -0,0 +1,24 @@
+//  Array
+
+const days = ["Monday", "Tuesday", "Wednesday"]
+
+console.log(days);
+console.log(`Type of : ${typeof days}`);
+console.log(`Total values in array : ${days.length}`);
+console.log(`First data: ${days[0]}`);
+console.log(`Last data: ${days[days.length-1]}`);
+
+
+// Array methods ( array.push(value), array.unshift(value), array.pop(), array.shift())
+
+days.push("Thursday");
+console.log(days);
+
+days.unshift("Sunday");
+console.log(days);
+
+days.pop();
+console.log(days);
+
+days.shift();
+console.log(days);
