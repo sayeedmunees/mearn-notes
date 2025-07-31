596235dbce178ab50452b081c6ebd943749e741b
Sayeed Munees <sayeedmunees0@gmail.com>
2025-07-31 08:48:13 +0530

    updated

---
+++ JS/a9.js
@@ -0,0 +1,12 @@
+// Nested Array
+// print number < 10
+
+var nestedArray = [[10,20], [30,2,30], [23,9,8,5] [23,45,54]]
+
+for(let subArray of nestedArray){
+    for (let num of subArray){
+        if (num < 10){
+            console.log();
+        }
+    }
+}
