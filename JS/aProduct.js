f7444bfc48e0bc8d3312ebf5ffc03d8fa0e401e4
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-01 11:04:09 +0530

    updated

---
+++ JS/aProduct.js
@@ -0,0 +1,37 @@
+// [id,name,price,stock]
+
+products =[
+
+    [1,'hide and seek',50,20],
+    [2,'lays',20,80],
+    [3,'oreo',40,100],
+    [4,'parleG',25,0],
+    [5,'tiger',20,30],
+    [6,'unibic',60,20],
+    [7,'good day',70,20],
+
+]
+
+
+
+// 1. display all product name : forEach
+
+
+
+// 2. display product whose price < rs.50 : filter
+
+// 3. print price of oreo : find
+
+// 4.print costly product name: reduce
+
+// 5. display out of stock product name: filter
+
+// 6. sort products based on stock  in decending order: sort
+
+// 7. print product having maximum availabe stock: reduce
+
+// 8. Is there any product can be purchased by Rs. 10: some
+
+// 9. Is there any product in the range of 10 to 30 : some
+
+// 10. Print all products in the range of 10 to 30: filter
