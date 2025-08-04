3b3212679fd04325a69297ea0759040f03e2069f
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-04 10:11:37 +0530

    updated

---
+++ JS/a13.js
@@ -0,0 +1,13 @@
+const nameArray = ["Mini", "Sini", "Rini", "Tini", "Sani"]
+
+// Is "Rini" is present in the array or not : includes
+console. log(nameArray.includes ("Rini"));
+
+// Display the index of "Rini"
+console. log(nameArray.indexOf("Rini"));
+
+// Remove "Rini: from the above array
+
+console. log (nameArray);
+console. log(nameArray.splice(2,1));
+console. log (nameArray) ;
