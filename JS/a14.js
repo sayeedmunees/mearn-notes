2a4a1d2df7ac0f93166c299937f010e9ffe7faee
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-05 09:13:21 +0530

    updated

--- JS/a14.js
+++ JS/a14.js
@@ -3,3 +3,8 @@ var str = "Malayalam";
 console.log(`Total Charecters: ${str.length}`);
 console.log(`Capital Letters: ${str.toUpperCase()}`);
 console.log(`Lower case Letters: ${str. toLowerCase()}`);
+
+console. log(`SubString of ${str}: ${str.substring(0,3)}`);
+console. log(`SubString of ${str}: ${str.slice(0,-2)}`);
+console. log(`Is ${str} starts with "M": ${str.startsWith("M")}`);
+console. log(`Is ${str} Ends with "M": ${str.endsWith ("M")}`);
