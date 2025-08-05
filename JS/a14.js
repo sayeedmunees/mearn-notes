f77dd298f3b7859118b67b4046c09fe570f3e110
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-05 09:48:29 +0530

    updated

--- JS/a14.js
+++ JS/a14.js
@@ -2,9 +2,18 @@ var str = "Malayalam";
 
 console.log(`Total Charecters: ${str.length}`);
 console.log(`Capital Letters: ${str.toUpperCase()}`);
-console.log(`Lower case Letters: ${str. toLowerCase()}`);
+console.log(`Lower case Letters: ${str.toLowerCase()}`);
 
 console. log(`SubString of ${str}: ${str.substring(0,3)}`);
 console. log(`SubString of ${str}: ${str.slice(0,-2)}`);
 console. log(`Is ${str} starts with "M": ${str.startsWith("M")}`);
 console. log(`Is ${str} Ends with "M": ${str.endsWith ("M")}`);
+
+var sentence = "Language is JS";
+console.log(sentence.split(' '));
+console.log(sentence.split(''));
+console.log(`Does str starts with Ma: ${str.includes('Ma')}`);
+
+var word = "    Hello World    "
+console.log(`Word length before: ${word.length}`);
+console.log(`Word length after: ${word.trim().length}`);
