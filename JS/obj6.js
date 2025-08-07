aae4e6ab7960170968af52e33dbf1ba8f0716de4
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-07 09:23:04 +0530

    updated

---
+++ JS/obj6.js
@@ -0,0 +1,14 @@
+weatherData=[
+    {district:"Trissur",temp:32},
+    {district:"Trissur",temp:28},
+    {district:"Kottayam",temp:29},
+    {district:"Palakakd",temp:34},
+    {district:"Ernakulam",temp:33},
+    {district:"Trissur",temp:29},
+    {district:"Kottayam",temp:30},
+    {district:"Palakakd",temp:32},
+    {district:"Ernakulam",temp:31}
+]
+
+//  print district with its highest temperature
+// output : {Trissur:32,Kottayam:30,....}
