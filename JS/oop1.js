1d014783b6bfc3c0dfba578e0c0a50f1eebaae89
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-08 11:02:10 +0530

    added oop1.js, introduced Object Oriented Programming

---
+++ JS/oop1.js
@@ -0,0 +1,18 @@
+class Employee{
+    // properties
+
+    // methods
+    constructor (id, name, salary, desig){
+        this. empid = id
+        this. empName = name
+        this. empSalary = salary
+        this. empDesig = desig
+    }
+    displayEmp(){
+        console. log(`EmpName is : ${this. empName}`);
+    }
+}
+
+// object
+const emp1 = new Employee(1, "Sulu", 23000, "Tester");
+emp1.displayEmp();
