f35ba79d1c3f33b81ec16e8fc9d9e295e5c45894
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-11 09:30:13 +0530

    Updated Commit

--- JS/oop1.js
+++ JS/oop1.js
@@ -19,3 +19,24 @@ emp1.displayEmp();
 
 const emp2 = new Employee(2, "Raju", 33000, "Developer");
 emp2.displayEmp();
+
+// create a class for product (id, name, price, quantity) in it a function for display product details and an object for that.
+
+class Products {
+    constructor(id, name, price, quantity) {
+        this.prodId = id
+        this.prodName = name
+        this.prodPrice = price
+        this.prodQuantity = quantity
+    }
+
+    displayProduct(){
+        console. log(`Id is : ${this.prodId}`);
+        console. log(`Name is : ${this.prodName}`);
+        console. log(`Price is : ${this.prodPrice}`);
+        console. log(`Quantity is : ${this.prodQuantity}`);
+    }
+}
+
+const prod1 = new Products(1,'Biscuit', 20, 2)
+prod1.displayProduct();
