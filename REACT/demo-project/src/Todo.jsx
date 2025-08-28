211cc1af743ddd20784e60a9924f8b1737d31086
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-28 09:07:44 +0530

    Update

--- REACT/demo-project/src/Todo.jsx
+++ REACT/demo-project/src/Todo.jsx
@@ -3,9 +3,16 @@ import useFetch from './useFetch'
 
 const Todo = () => {
     const allTodos = useFetch("https://jsonplaceholder.typicode.com/todos")
+    console.log(allTodos);
+
   return (
     <div>
         <h1 className='text-center my-5'>All Todos</h1>
+        {
+            allTodos?.map(item => (
+                <h4 id={item?.id}>{item?.title}</h4>
+            ))
+        }
     </div>
   )
 }
