24cad23f52421f76578dcbb5e961fe52877aab3e
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-28 09:59:22 +0530

    Added interest-app

--- REACT/interest-app/src/App.jsx
+++ REACT/interest-app/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{width:"100%", minHeight:"100vh"}}className='d-flex justify-content-center align-items-center bg-dark'>
+        <div style={{width:"600px"}} className='bg-light rounded p-5'>
+          <h1>Simple Interest Calculator</h1>
+          <h3>Calculate yourr simple interest easily</h3>
+          <div className='bg-warning p-3 text-light text-center rounded'>
+            <h1>₹1500</h1>
+            <p className='fw-bolder'>Total Simple Interest</p>
+          </div>
+          </div>
+        </div>
     </>
   )
 }
