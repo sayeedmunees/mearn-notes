36721e1601c8b3bfac16bfaf1a41c9d16d9de214
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-28 10:47:21 +0530

    Styled interest-app

--- REACT/interest-app/src/App.jsx
+++ REACT/interest-app/src/App.jsx
@@ -1,21 +1,77 @@
-import './App.css'
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [interest, setInterest] = useState(0);
+  const [principle, setPrinciple] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [year, setYear] = useState(0);
 
   return (
     <>
-      <div style={{width:"100%", minHeight:"100vh"}}className='d-flex justify-content-center align-items-center bg-dark'>
-        <div style={{width:"600px"}} className='bg-light rounded p-5'>
-          <h1>Simple Interest Calculator</h1>
-          <h3>Calculate yourr simple interest easily</h3>
-          <div className='bg-warning p-3 text-light text-center rounded'>
+      <div
+        style={{ width: "100%", minHeight: "100vh" }}
+        className="d-flex justify-content-center align-items-center bg-dark"
+      >
+        <div style={{ width: "600px" }} className="bg-light rounded p-5">
+          <h2 className="text-center">Simple Interest Calculator</h2>
+          <p className="text-center">Calculate your simple interest easily</p>
+          <div className="bg-warning p-3 text-light text-center rounded">
             <h1>₹1500</h1>
-            <p className='fw-bolder'>Total Simple Interest</p>
+            <p className="fw-bolder">Total Simple Interest</p>
           </div>
-          </div>
+          <form className="mt-3">
+            <div className="mb-3">
+              {/* Principle */}
+              <TextField
+                className="w-100"
+                id="outlined-priciple"
+                label="₹ Principle Amount"
+                variant="outlined"
+              />
+            </div>
+            {/* Rate */}
+            <div className="mb-3">
+              <TextField
+                className="w-100"
+                id="outlined-rate"
+                label="% Rate of Interest"
+                variant="outlined"
+              />
+            </div>
+            {/* Year */}
+            <div className="mb-3">
+              <TextField
+                className="w-100"
+                id="outlined-year"
+                label="Year (yr)"
+                variant="outlined"
+              />
+            </div>
+            <Stack direction="row" spacing={2}>
+              <Button
+                className="bg-dark py-3 shadow"
+                style={{ width: "50%" }}
+                variant="contained"
+              >
+                CALCULATE
+              </Button>
+              <Button
+                className="border border-dark text-dark py-3 shadow"
+                style={{ width: "50%" }}
+                variant="outlined"
+              >
+                RESET
+              </Button>
+            </Stack>
+          </form>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
