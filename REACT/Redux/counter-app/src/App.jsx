6592de4f8a7b37be1b1ba68fb61347855918e11d
Sayeed Munees <sayeedmunees0@gmail.com>
2025-09-24 10:00:10 +0530

    Created counter components

--- REACT/Redux/counter-app/src/App.jsx
+++ REACT/Redux/counter-app/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Counter from "./components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="text-center mt-5">Counter App</h1>
+      <div
+        style={{ minHeight: "70vh", border: "1px solid white" }}
+        className="d-flex justify-content-center align-items-center w-100"
+      >
+        <Counter />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
