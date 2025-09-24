7dbec946c4d0cc2b0d4e78ab11efadc304e519e8
Sayeed Munees <sayeedmunees0@gmail.com>
2025-09-24 09:43:38 +0530

    Created template for a counter app using redux

--- REACT/RBuilder/rBuilder-frontend/src/pages/PageNotFound.jsx
+++ REACT/RBuilder/rBuilder-frontend/src/pages/PageNotFound.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <div>
+      <img
+        src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2loeXR0eWF0cmpkYWs5eDhpbjI4Z3lxaGxza3hkams1dnJyNDNxYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gX3u0staej6wAnRlcP/giphy.gif"
+        alt="gif"
+      />
+    </div>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
