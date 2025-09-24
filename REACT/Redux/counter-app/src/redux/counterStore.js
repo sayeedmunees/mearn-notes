d061e78e6113c06263de902498f39c2691d10e2b
Sayeed Munees <sayeedmunees0@gmail.com>
2025-09-24 10:59:52 +0530

    Added reduc files

--- REACT/Redux/counter-app/src/redux/counterStore.js
+++ REACT/Redux/counter-app/src/redux/counterStore.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counterSlice";
 
 // define logic to create redux store
 const counterStore = configureStore({
-  reducer: {},
+  reducer: {
+    counterReducer: counterSlice,
+  },
 });
 
 export default counterStore;
