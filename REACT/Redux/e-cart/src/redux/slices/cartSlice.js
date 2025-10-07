9088790aa18f4dc9acdf6cdca8e3978000659f39
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-07 11:03:40 +0530

    Updated

--- REACT/Redux/e-cart/src/redux/slices/cartSlice.js
+++ REACT/Redux/e-cart/src/redux/slices/cartSlice.js
@@ -1,11 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import wishlistSlice from "./wishlistSlice";
 
 const cartSlice = createSlice({
   name: "cartItems",
   initialState: [],
   reducers: {
     addToCart: (state, actionFromComponent) => {
-      state.push(actionFromComponent.payload);
+      const existingProduct = state.find(
+        (item) => item.id == actionFromComponent.payload.id
+      );
+      if (existingProduct) {
+        existingProduct.quantity++;
+        existingProduct.totalPrice =
+          existingProduct.quantity * existingProduct.price;
+        const remainingProducts = state.filter(
+          (item) => item.id != existingProduct.id
+        );
+        state = [...remainingProducts, existingProduct];
+      } else {
+        state.push({
+          ...actionFromComponent.payload,
+          quantity: 1,
+          totalPrice: actionFromComponent.payload.price,
+        });
+      }
     },
   },
 });
