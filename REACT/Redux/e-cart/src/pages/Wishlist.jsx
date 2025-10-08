ed240ec37eb7a8facb203f6a78db1cb1350b40e2
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-08 09:42:20 +0530

    Updated

--- REACT/Redux/e-cart/src/pages/Wishlist.jsx
+++ REACT/Redux/e-cart/src/pages/Wishlist.jsx
@@ -3,10 +3,24 @@ import Header from "../components/Header";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { removeItem } from "../redux/slices/wishlistSlice";
+import { addToCart } from "../redux/slices/cartSlice";
 
 const Wishlist = () => {
-  const userWishlist = useSelector((state) => state.wishlistReducer);
   const dispatch = useDispatch();
+  const userCart = useSelector((state) => state.cartReducer);
+  const userWishlist = useSelector((state) => state.wishlistReducer);
+
+  const handleCart = (product) => {
+    dispatch(addToCart(product));
+    const existingProduct = userCart?.find((item) => item?.id == id);
+    if (existingProduct) {
+      alert("Product Quantity Increased!!!!");
+    } else {
+      alert("Product added to Cart!!!!");
+    }
+    dispatch(removeItem(product?.id));
+  };
+
   return (
     <>
       <Header />
@@ -32,7 +46,10 @@ const Wishlist = () => {
                       >
                         <i className="fa-solid fa-heart-circle-xmark text-red-500"></i>
                       </button>
-                      <button className="text-xl">
+                      <button
+                        onClick={() => handleCart(product)}
+                        className="text-xl"
+                      >
                         <i className="fa-solid fa-cart-plus text-green-500"></i>
                       </button>
                     </div>
