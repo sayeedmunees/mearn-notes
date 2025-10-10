4fc1eb0d5a8bca1f9b1fc217f917e144afdf3923
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-10 10:42:48 +0530

    installed and added google fonts, font awesome, tailwind css, few pages and componenets

--- REACT/bookstore/frontend/src/pages/PageNotFound.jsx
+++ REACT/bookstore/frontend/src/pages/PageNotFound.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <>
-  <div className="w-full h-screen "></div>
-  </>;
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="md:grid grid-cols-3">
+          <div></div>
+          <div>
+            <img
+              src="https://miro.medium.com/v2/resize:fit:1400/0*GUYQoLJ08bNdTigR.gif"
+              alt="Oops.."
+            />
+            <p className="text-center">Oh No!</p>
+            <h2 className="md:text-5xl text-2xl text-center">
+              Looks like you're lost
+            </h2>
+            <h5 className="text-center">
+              The page you're looking for is not available
+            </h5>
+            <Link className="text-center" to={"/"}>
+              <button className="mt-4 px-4 text-center py-3 bg-blue-800 text-white rounded hover:border hover:border-blue-800 hover:bg-white hover:text-blue-800">
+                Back Home
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PageNotFound;
