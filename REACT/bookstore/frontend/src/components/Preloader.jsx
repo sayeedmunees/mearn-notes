189a8c808f8bebd0acdf5832d4bba0efb15d13f0
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-13 10:02:13 +0530

    Updated

--- REACT/bookstore/frontend/src/components/Preloader.jsx
+++ REACT/bookstore/frontend/src/components/Preloader.jsx
@@ -9,7 +9,7 @@ const Preloader = () => {
           <div></div>
           <div className="flex flex-col justify-center items-center">
             <img
-              src="https://media.tenor.com/_m9BeUagaMkAAAAi/writing-hand-joypixels.gif"
+              src="https://i.pinimg.com/originals/e1/59/25/e15925c931a81678a3c2e0c0a40db781.gif"
               alt="Loading"
             />
           </div>
--- REACT/bookstore/frontend/src/users/components/Header.jsx
+++ REACT/bookstore/frontend/src/users/components/Header.jsx
@@ -3,25 +3,33 @@ import {
   faInstagram,
   faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
     <>
       <nav className="container mx-auto">
-        <div className="flex flex-row justify-between items-center mx-auto w-full h-20">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdAhTXETNIvE8PsF6Uc6bElMUo-GmSmCx01Q&s"
-            alt=""
-            className="h-12"
-          />
-          <h1 className="font-bold text-3xl">Book Store</h1>
-          <div className="inline-flex items-center gap-2">
-            <FontAwesomeIcon className="text-2xl" icon={faInstagram} />
-            <FontAwesomeIcon className="text-2xl" icon={faXTwitter} />
-            <FontAwesomeIcon className="text-2xl" icon={faFacebook} />
-            <button className="bg-amber-800 rounded-full h-8 w-8 flex justify-center items-center">
-              <h2 className=" text-white">L</h2>
+        <div className="flex flex-row justify-between items-center w-full h-20 px-4">
+          <div>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdAhTXETNIvE8PsF6Uc6bElMUo-GmSmCx01Q&s"
+              alt=""
+              className="h-12 w-auto md:h-14"
+            />
+          </div>
+          <div>
+            <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl uppercase">
+              Book Store
+            </h1>
+          </div>
+          <div className="hidden md:inline-flex items-center gap-2">
+            <FontAwesomeIcon className="text-xl" icon={faInstagram} />
+            <FontAwesomeIcon className="text-xl" icon={faXTwitter} />
+            <FontAwesomeIcon className="text-xl" icon={faFacebook} />
+            <button className="border border-gray-700 bg-white rounded-lg h-8 w-fit p-3 flex justify-center items-center hover:bg-gray-800 text-gray-900 hover:text-white">
+              <FontAwesomeIcon icon={faUser} />
+              <h2>Login</h2>
             </button>
           </div>
         </div>
