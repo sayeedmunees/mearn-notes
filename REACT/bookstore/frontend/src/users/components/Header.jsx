99c7f8e9ac87f41837c40dfabb31cccff691cf2c
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-13 10:20:03 +0530

    Updated

--- REACT/bookstore/frontend/src/users/components/Header.jsx
+++ REACT/bookstore/frontend/src/users/components/Header.jsx
@@ -3,14 +3,16 @@ import {
   faInstagram,
   faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const Header = () => {
+  const [status, setStatus] = useState(false);
   return (
     <>
-      <nav className="container mx-auto">
-        <div className="flex flex-row justify-between items-center w-full h-20 px-4">
+      <nav className="mx-auto">
+        <div className="flex flex-row justify-start md:justify-between items-center w-full h-20 px-4 gap-4">
           <div>
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdAhTXETNIvE8PsF6Uc6bElMUo-GmSmCx01Q&s"
@@ -31,6 +33,31 @@ const Header = () => {
               <FontAwesomeIcon icon={faUser} />
               <h2>Login</h2>
             </button>
+          </div>
+        </div>
+
+        <div className=" bg-green-950 flex flex-col py-4 gap-2 ">
+          <div className="flex justify-between px-4 md:hidden">
+            <button
+              className="rounded-lg h-8 w-fit p-3 flex justify-center items-center text-white"
+              onClick={() => setStatus(!status)}
+            >
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+            <button className="rounded-lg h-8 w-fit p-3 flex justify-center items-center text-white">
+              <FontAwesomeIcon icon={faUser} />
+              <h2>Login</h2>
+            </button>
+          </div>
+          <div className="flex justify-center items-center text-white">
+            <ul
+              className={status ? "md:flex" : "md:flex gap-4 justify-center hidden"}
+            >
+              <li>Home</li>
+              <li>Books</li>
+              <li>Career</li>
+              <li>Contact</li>
+            </ul>
           </div>
         </div>
       </nav>
