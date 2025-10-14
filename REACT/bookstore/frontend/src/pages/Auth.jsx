53f7c8a045c221b8697663b26fcb360889d4a4f9
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-14 09:53:47 +0530

    Updated

--- REACT/bookstore/frontend/src/pages/Auth.jsx
+++ REACT/bookstore/frontend/src/pages/Auth.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import {
+  faCircleUser,
+  faEyeSlash,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const Auth = () => {
   return (
-    <div>Auth</div>
-  )
-}
+    <div id="auth" className="flex justify-center items-center w-full">
+      <div className="bg-green-950 rounded-xl text-white text-center gap-4 flex flex-col justify-center items-center p-10 ">
+        <div className=" flex justify-center items-center text-center flex-col px-5">
+          <FontAwesomeIcon className="text-9xl" icon={faCircleUser} />
+          <h2 className="text-2xl mt-4 font-bold">Register</h2>
+        </div>
+        <div className="flex w-full bg-white rounded items-center py-2 px-4 ">
+          <input
+            type="text"
+            placeholder="Username"
+            className=" placeholder-gray-600 w-full"
+          />
+        </div>
+        <div className="flex w-full bg-white rounded items-center py-2 px-4 ">
+          <input
+            type="text"
+            placeholder="Email"
+            className=" placeholder-gray-600 w-full"
+          />
+        </div>
+        <div>
+          <div className="flex w-full bg-white rounded items-center py-2 px-4 ">
+            <input
+              type="text"
+              placeholder="Password"
+              className=" placeholder-gray-600 w-full"
+            />
+            <FontAwesomeIcon className="text-green-950" icon={faEyeSlash} />
+          </div>
+          <p>*Never share password with others.</p>
+        </div>
 
-export default Auth
+        <button className="bg-white border border-white hover:bg-green-950 rounded hover:text-white text-green-950 px-4 py-2">
+          Register
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
