f783914ec62239a4e4586ca552c09c3b9b436dc2
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-20 15:42:01 +0530

    Developed home design

--- REACT/bookstore/frontend/src/admin/pages/AdminHome.jsx
+++ REACT/bookstore/frontend/src/admin/pages/AdminHome.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import AdminHeader from "../components/AdminHeader";
 import Footer from "../../components/Footer";
 import AdminSidebar from "../components/AdminSidebar";
+import { faBook, faUsers, faUserTie } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AdminHome = () => {
   return (
@@ -12,7 +14,28 @@ const AdminHome = () => {
         <div className="bg-green-100 flex flex-col items-center">
           <AdminSidebar />
         </div>
-        <div>Home</div>
+        <div className="flex gap-2 justify-around mt-4">
+          <div className="flex gap-2  items-center px-6 py-4 bg-green-950 text-white rounded-xl h-fit">
+            <FontAwesomeIcon className="text-4xl" icon={faBook} />
+            <p className="flex flex-col text-center">
+              Total Number of Books <span className="font-bold text-xl">100+</span>
+            </p>
+          </div>
+
+          <div className="flex gap-2 items-center px-6 py-4 bg-blue-950 text-white rounded-xl h-fit">
+            <FontAwesomeIcon className="text-4xl" icon={faUsers} />
+            <p className="flex flex-col text-center">
+              Total Number of Users <span className="font-bold text-xl">100+</span>
+            </p>
+          </div>
+
+          <div className="flex gap-2  items-center px-6 py-4 bg-amber-700 text-white rounded-xl h-fit">
+            <FontAwesomeIcon className="text-4xl" icon={faUserTie} />
+            <p className="flex flex-col text-center">
+              Total Number of Employees <span className="font-bold text-xl">100+</span>
+            </p>
+          </div>
+        </div>
       </div>
 
       <Footer />
