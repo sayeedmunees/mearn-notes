aa7c290c2188648c375feb5f97217417bcc09031
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-21 08:56:59 +0530

    Developed admin design

--- REACT/bookstore/frontend/src/admin/components/AdminHeader.jsx
+++ REACT/bookstore/frontend/src/admin/components/AdminHeader.jsx
@@ -19,7 +19,7 @@ const AdminHeader = () => {
             </span>
           </h1>
         </div>
-        <button className="px-4 py-2 text-green-950 border border-green-950 rounded hover:bg-green-950 hover:text-white">
+        <button className="px-4 py-2 font-semibold text-green-950 border border-green-950 rounded hover:bg-red-700 hover:text-white">
           <FontAwesomeIcon icon={faPowerOff} /> Logout
         </button>
       </div>
