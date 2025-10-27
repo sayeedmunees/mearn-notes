5f6dd6520a8e7122b2b4aa6b727c45c6aaf24e09
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-27 11:04:59 +0530

    Updated

--- REACT/bookstore/backend/controller/userController.js
+++ REACT/bookstore/backend/controller/userController.js
@@ -1,6 +1,23 @@
 // register
 
-exports.registerController = (req, res) => {
+const users = require("../model/userModel");
+
+exports.registerController = async (req, res) => {
   // logic
-  res.status(200).json("request recieved");
+  const { username, email, password } = req.body;
+  console.log({ username, email, password });
+  try {
+    const existingUser = await users.findOne({ email });
+    if (existingUser) {
+      res.status(400).json("Existing User");
+    } else {
+      const newUser = new users({
+        username,
+        email,
+        password,
+      });
+      await newUser.save(); // mongodb save
+      res.status(200).json(newUser);
+    }
+  } catch (err) {}
 };
