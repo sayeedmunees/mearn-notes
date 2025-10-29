e6d5b42c186489b71ab95ed5fd0d59c555fe3b50
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-29 10:51:56 +0530

    Updated

--- REACT/bookstore/backend/controller/userController.js
+++ REACT/bookstore/backend/controller/userController.js
@@ -1,6 +1,7 @@
 // register
 
 const users = require("../model/userModel");
+var jwt = require("jsonwebtoken");
 
 // register
 exports.registerController = async (req, res) => {
@@ -25,16 +26,16 @@ exports.registerController = async (req, res) => {
   }
 };
 
-// register
+// login
 exports.loginController = async (req, res) => {
-  // logic
   const { email, password } = req.body;
   console.log({ email, password });
   try {
     const existingUser = await users.findOne({ email });
     if (existingUser) {
       if (existingUser.password == password) {
-        res.status(200).json(existingUser);
+        const token = jwt.sign({userMail:existingUser.email}, "secretkey")
+        res.status(200).json({existingUser, token});
       } else {
         res.status(401).json("Incorrect Password");
       }
