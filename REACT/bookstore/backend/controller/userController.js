fa3deee3abe81b834eb8e1106e8bbe5df9bf07e6
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-29 10:20:50 +0530

    Updated

--- REACT/bookstore/backend/controller/userController.js
+++ REACT/bookstore/backend/controller/userController.js
@@ -2,6 +2,7 @@
 
 const users = require("../model/userModel");
 
+// register
 exports.registerController = async (req, res) => {
   // logic
   const { username, email, password } = req.body;
@@ -19,5 +20,28 @@ exports.registerController = async (req, res) => {
       await newUser.save(); // mongodb save
       res.status(200).json(newUser);
     }
-  } catch (err) {}
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// register
+exports.loginController = async (req, res) => {
+  // logic
+  const { email, password } = req.body;
+  console.log({ email, password });
+  try {
+    const existingUser = await users.findOne({ email });
+    if (existingUser) {
+      if (existingUser.password == password) {
+        res.status(200).json(existingUser);
+      } else {
+        res.status(401).json("Incorrect Password");
+      }
+    } else {
+      res.status(404).json("User doesn't exists");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
--- REACT/bookstore/backend/routes.js
+++ REACT/bookstore/backend/routes.js
@@ -3,11 +3,15 @@ const express = require("express");
 // import userController
 const userController = require("./controller/userController.js");
 
+
 // create instance
 const route = new express.Router();
 
-// path..
+// path for register
 route.post("/register", userController.registerController);
+
+// path for login
+route.post("/login", userController.loginController);
 
 // routes export
 module.exports = route;
